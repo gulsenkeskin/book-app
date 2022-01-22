44abf95087e1d54c711a91167f5fa90b95b81aa8
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-22 20:26:31 +0300

    Update theme.dart

--- lib/src/theme/theme.dart
+++ lib/src/theme/theme.dart
@@ -6,4 +6,10 @@ class Theme {
       colorScheme: const ColorScheme.light().copyWith(
         secondary: Colors.grey[300],
       ));
+
+  static final darkTheme = ThemeData(
+      scaffoldBackgroundColor: Colors.grey[900],
+      colorScheme: const ColorScheme.dark().copyWith(
+        secondary: Colors.black38,
+      ));
 }

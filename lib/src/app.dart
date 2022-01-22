e520c386fe78a5889c88acefec1465925f29033c
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-22 20:26:23 +0300

    Update app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,7 @@
+import 'package:book_app/src/page/home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-
-import 'sample_feature/sample_item_details_view.dart';
-import 'sample_feature/sample_item_list_view.dart';
 import 'settings/settings_controller.dart';
 import 'settings/settings_view.dart';
 
@@ -69,11 +67,9 @@ class MyApp extends StatelessWidget {
                 switch (routeSettings.name) {
                   case SettingsView.routeName:
                     return SettingsView(controller: settingsController);
-                  case SampleItemDetailsView.routeName:
-                    return const SampleItemDetailsView();
-                  case SampleItemListView.routeName:
+
                   default:
-                    return const SampleItemListView();
+                    return HomePage(settingsController: settingsController);
                 }
               },
             );

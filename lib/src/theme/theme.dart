dbb23b89c8a2a995624d1fa519ecdf42aa420615
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-22 20:18:05 +0300

    added lightTheme

--- lib/src/theme/theme.dart
+++ lib/src/theme/theme.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
-class Theme{
-
+class Theme {
+  static final lightTheme = ThemeData(
+      scaffoldBackgroundColor: Colors.grey[100],
+      colorScheme: const ColorScheme.light().copyWith(
+        secondary: Colors.grey[300],
+      ));
 }

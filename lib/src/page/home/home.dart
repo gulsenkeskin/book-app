7a10b192de9f271b2d3946eb840fca1a5c151fa4
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-22 20:26:27 +0300

    Create home.dart

---
+++ lib/src/page/home/home.dart
@@ -0,0 +1,13 @@
+import 'package:book_app/src/settings/settings_controller.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final SettingsController settingsController;
+  const HomePage({Key? key, required this.settingsController})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

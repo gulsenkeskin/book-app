c84ab5f598d74076cc6867c64a24bdc69f243f18
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-22 19:31:43 +0300

    add images

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_gen/gen_l10/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'sample_feature/sample_item_details_view.dart';

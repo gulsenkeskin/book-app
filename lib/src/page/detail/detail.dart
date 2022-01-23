4e150e7d8017da4b9a1358cbf2e5b6cd3f8a9278
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-24 01:43:32 +0300

    Update detail.dart
    yuvarlak geri butonu eklendi :octocat:

--- lib/src/page/detail/detail.dart
+++ lib/src/page/detail/detail.dart
@@ -20,9 +20,26 @@ class DetailPage extends StatelessWidget {
                 ClipRRect(
                   borderRadius:
                       const BorderRadius.vertical(bottom: Radius.circular(30)),
-                  child: Image.asset(book.imgUrl!,
-                  fit: BoxFit.fitWidth,),
+                  child: Image.asset(
+                    book.imgUrl!,
+                    fit: BoxFit.fitWidth,
+                  ),
                 ),
+                Positioned(
+                    top: 56,
+                    left: 20,
+                    child: GestureDetector(
+                      onTap: () => Navigator.of(context).pop(),
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Colors.grey[900], shape: BoxShape.circle),
+                        child: const Icon(
+                          Icons.arrow_back_rounded,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ))
               ],
             ),
           )

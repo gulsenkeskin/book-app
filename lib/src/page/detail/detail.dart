d4c3ac9572305a2261111f9a5dfc4495b6935f63
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-24 01:39:32 +0300

    Update detail.dart
       ClipRRect(
                      borderRadius:
                          const BorderRadius.vertical(bottom: Radius.circular(30)),
    
    sadece altk kısma border radius.circular ekler

--- lib/src/page/detail/detail.dart
+++ lib/src/page/detail/detail.dart
@@ -1,15 +1,32 @@
 import 'package:book_app/src/model/book.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
   final Book book;
-  const DetailPage(this.book,{Key? key}) : super(key: key);
+  const DetailPage(this.book, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          SizedBox(
+            height: 550,
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                ClipRRect(
+                  borderRadius:
+                      const BorderRadius.vertical(bottom: Radius.circular(30)),
+                  child: Image.asset(book.imgUrl!,
+                  fit: BoxFit.fitWidth,),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
